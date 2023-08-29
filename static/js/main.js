3b84ecf42036eec5f70ce2bf377ca04c8543aa0a
saihanhtet <hanhtetsan13@gmail.com>
2023-08-29 16:10:30 +0630

    links with sections added :)

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,25 @@ $(window).scroll(function () {
   $("nav").toggleClass("scrolled", $(this).scrollTop() > 0);
   $(".footer").toggleClass("scrolled", $(this).scrollTop() > 0);
 });
+
+const current_section_hash = window.location.hash;
+
+if (current_section_hash) {
+  const sectionElement = document.querySelector(current_section_hash);
+  if (sectionElement) {
+    sectionElement.scrollIntoView({ behavior: "smooth" });
+  }
+}
+const linksWithHash = document.querySelectorAll('a[href^="#"]');
+
+linksWithHash.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    // console.log(targetId, targetElement);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
